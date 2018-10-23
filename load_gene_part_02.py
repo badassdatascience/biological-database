359580e3e4e149cfc22f1512d9d7f2fe22bf474a
EC2 Default User <ec2-user@ip-172-31-47-104.us-west-2.compute.internal>
2018-10-23 20:18:37 +0000

    Initial import.

---
+++ load_gene_part_02.py
@@ -0,0 +1,62 @@
+
+#
+# import useful libraries
+#
+import pprint as pp
+import pickle
+
+#
+# user settings
+#
+gene_info_file = 'data/gene/gene_info'
+output_directory = 'output'
+
+#
+# initialize data structures
+#
+gene_info = {}
+
+#
+# iterate through each line in the gene_info file
+#
+f = open(gene_info_file)
+for line in f:
+    line = [x.strip() for x in line.split('\t')]
+
+    tax_id = line[0]
+
+    if tax_id == '#tax_id':
+        continue
+
+    tax_id = int(tax_id)
+    gene_id = int(line[1])
+    symbol = line[2]
+    synonyms = line[4]
+    type_of_gene = line[9]
+    name = line[11]
+
+    if symbol == '-':
+        symbol = None
+    if type_of_gene == '-':
+        type_of_gene = None
+    if name == '-':
+        name = None
+
+    cleaned_synonyms = [x for x in synonyms.split('|') if x != '-']
+
+    gene_info[gene_id] = {
+        'tax_id' : tax_id,
+        'symbol' : symbol,
+        'type_of_gene' : type_of_gene,
+        'name' : name,
+        'synonyms' : cleaned_synonyms,
+        }
+
+f.close()
+
+#
+# save our newly loaded data structures
+#
+with open(output_directory + '/gene_info.pickle', 'wb') as f:
+    pickle.dump(gene_info, f)
+
