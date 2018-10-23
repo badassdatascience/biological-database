a0a4dc7a0b6973b577168e7e28868e2eb821cd61
EC2 Default User <ec2-user@ip-172-31-47-106.us-west-2.compute.internal>
2018-10-23 21:57:39 +0000

    An adaption to deal with memory.

--- 01.py
+++ 01.py
@@ -4,20 +4,28 @@
 #
 import pprint as pp
 import pickle
+import uuid
+import os
 
 #
 # user settings
 #
 gene_info_file = 'data/gene/gene_info'
 output_directory = 'output'
+max_gene_info_list_size = 1000000
+
+#
+# CRUDELY clear the way
+#
+os.system('rm ' + output_directory + '/gene_lists/*')
 
 #
 # initialize data structures
 #
-gene_info = {}
 gene_to_tax_id = {}
 synonyms_to_tax_id = {}
 synonyms_to_gene_id = {}
+gene_info_list = []
 
 #
 # iterate through each line in the gene_info file
@@ -57,20 +65,24 @@ for line in f:
     if not gene_id in gene_to_tax_id:
         gene_to_tax_id[gene_id] = {}
     gene_to_tax_id[gene_id][tax_id] = None
-    
-    gene_info[gene_id] = {
-        'symbol' : symbol,
-        'type_of_gene' : type_of_gene,
-        'name' : name,
-        }
 
+    gene_info_list.append([gene_id, symbol, type_of_gene, name])
+    if len(gene_info_list) >= max_gene_info_list_size:
+        with open(output_directory + '/gene_lists/' + str(uuid.uuid4()) + '.pickle', 'wb') as f:
+            pickle.dump(gene_info_list, f)
+        gene_info_list = []
+    
 f.close()
+
+#
+# save our remaining gene list
+#
+with open(output_directory + '/gene_lists/' + str(uuid.uuid4()) + '.pickle', 'wb') as f:
+    pickle.dump(gene_info_list, f)
 
 #
 # save our newly loaded data structures
 #
-with open(output_directory + '/gene_info.pickle', 'wb') as f:
-    pickle.dump(gene_info, f)
 with open(output_directory + '/gene_to_tax_id.pickle', 'wb') as f:
     pickle.dump(gene_to_tax_id, f)
 with open(output_directory + '/synonyms_to_tax_id.pickle', 'wb') as f:
